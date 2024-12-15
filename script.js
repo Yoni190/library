c2ab7316a0fe6ccfb53035ac45681b309f5cd1ae
Yoni190 <yfisuhu@gmail.com>
2024-12-15 20:17:39 +0300

    Add event listener to add a book

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const main = document.querySelector('.main');
-
-const bookTest = {title: "The Hobbit", author: "JRR Tolkien", pages: 295};
+const addButton = document.querySelector('.addButton');
 
 const myLibrary = [bookTest];
 
@@ -26,14 +25,28 @@ function displayBookInfo(){
         author.textContent = myLibrary[i].author;
         const pages = document.createElement('p');
         pages.textContent = myLibrary[i].pages;
+        const isRead = document.createElement('button');
+        isRead.innerHTML = myLibrary[i].isRead ? "Not Read" : 'Read'; 
+        isRead.className = 'read';
         card.className = 'card';
 
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
+        card.appendChild(isRead);
         main.appendChild(card);
 
         console.log(myLibrary[i].title);
     }
 }
-displayBookInfo();
+
+addButton.addEventListener('click', ()=>{
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const isRead = document.querySelector('#read').checked;
+    console.log(isRead);
+
+    addBookToLibrary(title, author, pages, isRead);
+    displayBookInfo();
+});
