4a073fe24c4490fede4dcd1ff361871c7759907e
Yoni190 <yfisuhu@gmail.com>
2024-12-16 14:31:48 +0300

    Add remove button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const addButton = document.querySelector('.addButton');
 const showDialog = document.querySelector('.show-add-dialog');
 const closeButton = document.querySelector('.close');
 const dialog = document.querySelector('dialog');
+const inputs = document.querySelectorAll('input');
 
 const myLibrary = [];
 
@@ -25,21 +26,37 @@ function displayBookInfo(){
     }
     for(i = 0; i < myLibrary.length; i++){
         const card = document.createElement('div');
+        card.className = 'card';
+
         const title = document.createElement('p');
         title.textContent = myLibrary[i].title;
+
         const author = document.createElement('p');
         author.textContent = myLibrary[i].author;
+
         const pages = document.createElement('p');
         pages.textContent = myLibrary[i].pages;
+
         const isRead = document.createElement('button');
         isRead.innerHTML = myLibrary[i].isRead ? "Not Read" : 'Read'; 
         isRead.className = 'read';
-        card.className = 'card';
+
+        const index = i;
+
+        const removeButton = document.createElement('button');
+        removeButton.addEventListener('click', ()=>{
+            myLibrary.splice(index, 1);
+            displayBookInfo();
+        });
+
+        removeButton.innerHTML = "Remove Book";
+        
 
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
         card.appendChild(isRead);
+        card.appendChild(removeButton);
         main.appendChild(card);
 
         console.log(myLibrary);
@@ -62,6 +79,9 @@ addButton.addEventListener('click', ()=>{
     console.log(isRead);
 
     addBookToLibrary(title, author, pages, isRead);
+    inputs.forEach((input)=>{
+        input.value = '';
+    })
     dialog.close();
     displayBookInfo();
 });
